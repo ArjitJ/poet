897f9f83c1e3187783537e44151a80f8e2f4685d
Arjit Jain <thearjitjain@gmail.com>
2021-04-05 18:32:09 +0530

    Create minigrid_run_local_lite.sh

---
+++ poet/minigrid_run_local_lite.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# export PATH=/root/gym-minigrid:$PATH
+if [ -z "$1" ]
+then
+    echo "Missing an experiment id"
+    exit 1
+fi
+if [ -z "$2" ]
+then
+    echo "Setting iteration to default of 50000"
+    ITR=1000
+else
+    ITR=$2
+fi
+
+experiment=poet_$1
+
+mkdir -p poet_mini/$experiment/ipp
+mkdir -p poet_mini/$experiment/logs
+
+python3 -u minigrid_master.py \
+  poet_mini/$experiment/logs \
+  --init=random \
+  --learning_rate=0.01 \
+  --lr_decay=0.9999 \
+  --lr_limit=0.001 \
+  --batch_size=1 \
+  --batches_per_chunk=1 \
+  --eval_batch_size=1 \
+  --eval_batches_per_step=1 \
+  --master_seed=42 \
+  --noise_std=0.01 \
+  --noise_decay=0.999 \
+  --noise_limit=0.01 \
+  --repro_threshold 10 \
+  --mc_lower -20 \
+  --mc_upper 95 \
+  --normalize_grads_by_noise_std \
+  --returns_normalization=centered_ranks \
+  --stochastic \
+  --envs lava obstacle box_to_ball door wall  \
+  --max_num_envs=2 \
+  --adjust_interval=2 \
+  --propose_with_adam \
+  --steps_before_transfer=25 \
+  --num_workers 1 \
+  --n_iterations=$ITR 2>&1 | tee poet_mini/$experiment/ipp/run.log
